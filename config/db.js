566dba35cc015960cdd529b0420d3ac78dccd739
parita15 <parishah052@gmail.com>
2022-10-30 15:39:26 -0400

    uri chnage

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 module.exports = 
 {
     // "URI": "mongodb://127.0.0.1:27017/assignment2",
-    "URI": "mongodb+srv://pari:Parita@1502@cluster0.ffexnhk.mongodb.net/?retryWrites=true&w=majority",
+    "URI": "mongodb+srv://pari:Parita@1502@cluster0.ffexnhk.mongodb.net/assignment2?retryWrites=true&w=majority",
+    // mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority
     "Secret": 'SomeSecret'
 }
