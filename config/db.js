1f531f02cc0403712fc0a87dcb45c352632d2210
parita15 <parishah052@gmail.com>
2022-10-30 15:08:55 -0400

    change the live URL

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 module.exports = 
 {
-    "URI": "mongodb://127.0.0.1:27017/assignment2",
+    // "URI": "mongodb://127.0.0.1:27017/assignment2",
+    "URI": "mongodb+srv://pari:Parita@1502@cluster0.ffexnhk.mongodb.net/?retryWrites=true&w=majority",
     "Secret": 'SomeSecret'
 }
